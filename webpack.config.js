67492172d8648daed9b8e05295aefeca3e7a9691
augustinr <augustin.roux@gmail.com>
2021-04-21 19:29:19 +0200

    🔧 Webpack: Remove js files from babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(j|t)sx?$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
