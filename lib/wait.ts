f7053ed403f7f95a6ef11269796daf24df8aa2be
augustinr <augustin.roux@gmail.com>
2021-04-26 14:50:42 +0200

    ✨ Add wait helper

---
+++ lib/wait.ts
@@ -0,0 +1,10 @@
+const wait = async (delay: number) => (
+  new Promise((res) => {
+    let timeout = setTimeout(() => {
+      clearTimeout(timeout)
+      res(null)
+    }, delay)
+  })
+)
+
+export default wait
